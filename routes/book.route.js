d0685345b17cca359553a6dd843df363aa009be0
Imran shaikh <imranpathan7040@gmail.com>
2025-05-21 23:16:19 +0530

    getAllBook Route updated

--- routes/book.route.js
+++ routes/book.route.js
@@ -5,7 +5,7 @@ const upload = require("../middleware/uploadMiddleware");
 const router = express.Router();
 
 router.post("/books",authMiddleware,upload.single("image"),createBook)
-router.get("/books",authMiddleware,getAllBooks)
+router.get("/books",getAllBooks)
 router.get("/books/:id",authMiddleware,getBookDetails);
 router.get("/search",searchBooks);
 
